6627553c96882bb24188c9adb801c9f5f605d145
Devin Price <devin@wptheming.com>
2011-06-10 00:05:14 -0500

    Initial commit.

---
+++ post-loaded-avatars.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Post Loaded Avatars
+Plugin URI: http://www.wptheming.com
+Description: Delays the load of avatar images to help speed up the rest of your content.
+Version: 0.1
+Author: Devin Price
+Author URI: http://www.wptheming.com
+License: GPLv2
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+/* Basic plugin definitions */
+
+define('POST_LOADED_AVATARS', '0.1');
+
+/* Make sure we don't expose any info if called directly */
+
+if ( !function_exists( 'add_action' ) ) {
+	echo "Hi there!  I'm just a little plugin, don't mind me.";
+	exit;
+}
+
+function post_loaded_avatars() {
+	if (!is_admin()) {
+		wp_enqueue_script('jquery');
+		add_filter('get_avatar','post_load_avatar',10,5);
+		add_action('wp_print_footer_scripts', 'post_load_avatar_scripts');
+	}
+}
+
+add_action('init', 'post_loaded_avatars' );
+
+function post_load_avatar($avatar, $id_or_email, $size, $default, $alt) {
+	$avatar = str_replace('src','src="' . $default . '" data-src', $avatar);
+	return $avatar;
+}
+
+function post_load_avatar_scripts() { ?>
+	<script>
+        jQuery(window).load(function() {
+                jQuery('.avatar').each(function(index) {
+                    jQuery(this).hide().attr("src",jQuery(this).attr("data-src")).fadeIn();
+                });
+        });
+    </script>
+<?php }
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Post Loaded Avatars ===
+Contributors: Devin Price
+Tags: avatar, gravatar
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=X238BDP4QGTV2
+Requires at least: 3.0
+Tested up to: 3.2
+Stable tag: 0.1
+License: GPLv2
+
+== Description ==
+
+Post Loaded Avatars makes it so the avatar images on a page are the last items to load.  This clears the way for other content to load quicker and can decrease the page load time for Google.
+
+== Installation ==
+
+Just install the plugin.  No options to configure.
+
+== Frequently Asked Questions ==
+
+= Why isn't it working? =
+
+It might be working, it's just really fast.  If you view the source and find the term "data-src", it's probably working just fine.
+
+= No really, it's not working =
+
+There might be conflicts if you are using another lazy load script or if another plugin is applying filters to the comments.
+
+== Changelog ==
+
+= 0.1 =
+
+* Initial release
